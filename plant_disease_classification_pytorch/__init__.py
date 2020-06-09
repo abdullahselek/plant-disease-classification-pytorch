2eb7dc464498607eb67fb2e62e29aa6ce09f2a71
Abdullah Selek <abdullahselek@gmail.com>
2020-06-09 20:37:34 +0100

    Create main folder

---
+++ plant_disease_classification_pytorch/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''Disease classification on different plants with using Machine Learning and Convolutional Neural Networks.'''
+
+from __future__ import absolute_import
+
+__author__       = 'Abdullah Selek'
+__email__        = 'abdullahselek@gmail.com'
+__copyright__    = 'Copyright (c) 2020 Abdullah Selek'
+__license__      = 'MIT License'
+__version__      = '0.1'
+__url__          = 'https://github.com/abdullahselek/plant-disease-classification-pytorch'
+__download_url__ = 'https://github.com/abdullahselek/plant-disease-classification-pytorch'
+__description__  = 'Disease classification on different plants with using Machine Learning and Convolutional Neural Networks.'
