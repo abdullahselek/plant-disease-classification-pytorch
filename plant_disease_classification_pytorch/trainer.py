37fe444a169c2d4eabd5af725e53c4f88c578218
Abdullah Selek <abdullahselek@gmail.com>
2020-07-23 19:31:49 +0100

    Start working on train method

--- plant_disease_classification_pytorch/trainer.py
+++ plant_disease_classification_pytorch/trainer.py
@@ -4,6 +4,8 @@ import os
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import torch.optim as optim
+import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -32,4 +34,36 @@ def create_dataloaders():
     return trainloader, testloader
 
 
-net = Net()
+def train():
+    trainloader, testloader = create_dataloaders()
+
+    net = Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    for epoch in range(3):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+
+train()
