b5d7453726799545239b55521fc59cb7045df840
Abdullah Selek <abdullahselek@gmail.com>
2020-08-11 21:06:03 +0100

    Update networks

--- plant_disease_classification_pytorch/network.py
+++ plant_disease_classification_pytorch/network.py
@@ -8,9 +8,9 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv1d(3, 128, 128)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv1d(3, 128, 128)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
