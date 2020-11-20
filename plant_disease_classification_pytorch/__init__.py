1ecc99aae0e69264f18949a3ced66abb68bc5c69
Abdullah Selek <abdullahselek@gmail.com>
2020-11-20 18:30:03 +0000

    Fix download url

--- plant_disease_classification_pytorch/__init__.py
+++ plant_disease_classification_pytorch/__init__.py
@@ -10,9 +10,7 @@ __copyright__ = "Copyright (c) 2020 Abdullah Selek"
 __license__ = "MIT License"
 __version__ = "0.1"
 __url__ = "https://github.com/abdullahselek/plant-disease-classification-pytorch"
-__download_url__ = (
-    "https://github.com/abdullahselek/plant-disease-classification-pytorch"
-)
+__download_url__ = "https://github.com/abdullahselek/plant-disease-classification-pytorch"
 __description__ = "Disease classification on different plants with using Machine Learning and Convolutional Neural Networks."
 
 
