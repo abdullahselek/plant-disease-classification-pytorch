9fd3b9376f00b66ebb3e07943287867da1d554df
Abdullah Selek <abdullahselek@gmail.com>
2020-11-21 22:32:40 +0000

    Add new arguments

--- plant_disease_classification_pytorch/__main__.py
+++ plant_disease_classification_pytorch/__main__.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 
 from plant_disease_classification_pytorch import trainer
+from plant_disease_classification_pytorch.classifier import Classifier
 
 if __name__ == "__main__":
 
@@ -11,6 +12,8 @@ if __name__ == "__main__":
         description="Disease classification on different plants with using Machine Learning and Convolutional Neural Networks."
     )
     parser.add_argument("-t", "--train", type=int, help="Number of epochs for training")
+    parser.add_argument("-m", "--model", type=str, help="Path of the model will be used in classification")
+    parser.add_argument("-i", "--image", type=str, help="Path of the image file you want to classify")
     args = parser.parse_args()
 
     if len(sys.argv) < 2:
@@ -28,3 +31,7 @@ if __name__ == "__main__":
     if args.train != None:
         trainer.EPOCHS = args.train
         trainer.train()
+    if args.model != None and args.image != None:
+        classifier = Classifier(model_path=args.model)
+        prediction = classifier.classify(image_path=args.image)
+        print(prediction)
